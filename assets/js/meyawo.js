c29ba137c96d5d2a6cfb5541c7933601a8276a8f
SMA110 <smatechnology110@gmail.com>
2024-02-08 20:30:41 +0500

    uo

--- assets/js/meyawo.js
+++ assets/js/meyawo.js
@@ -1,19 +1,11 @@
-/*!
-=========================================================
-* Meyawo Landing page
-=========================================================
-
-* Copyright: 2019 DevCRUD (https://devcrud.com)
-* Licensed: (https://devcrud.com/licenses)
-* Coded by www.devcrud.com
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
 
 // smooth scroll
 $(document).ready(function(){
+    $("#msgdv").hide();    
+    $("#liveproj").hide();    
+    $("#KRIproj").hide();
+    $("#RSproj").hide();
+    
     $(".navbar .nav-link").on('click', function(event) {
 
         if (this.hash !== "") {
@@ -29,6 +21,16 @@ $(document).ready(function(){
             });
         } 
     });
+    const urlParams = new URLSearchParams(window.location.search);
+            const myParam = urlParams.get('PROJ');
+            if(myParam==='RES'){                
+                $("#RSproj").show();
+            }else if(myParam==='live'){
+                $("#liveproj").show();
+            }else if(myParam==='KRI'){
+                $("#KRIproj").show();
+            }
+
 });
 
 // navbar toggle
@@ -36,3 +38,50 @@ $('#nav-toggle').click(function(){
     $(this).toggleClass('is-active')
     $('ul.nav').toggleClass('show');
 });
+
+function SaveMsg(){
+    
+const dbName = "MyPortfolioDB";
+const dbVersion = 1;
+const storeName = "UsersMsg";
+
+// Open (or create) a database
+const request = indexedDB.open(dbName, 1);
+const nme = document.getElementById('nm').value;
+const em = document.getElementById('eml').value;
+const mess = document.getElementById('comment').value;
+
+// Handle database upgrade (create object store)
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    const objectStore = db.createObjectStore(storeName, { keyPath: 'id', autoIncrement: true });
+};
+
+// Handle successful database opening
+request.onsuccess = function(event) {
+    const db = event.target.result;
+    const transaction = db.transaction([storeName], 'readwrite');
+    const objectStore = transaction.objectStore(storeName);
+
+    // Add data
+    objectStore.add({ name: nme, email: em, Msg:mess});
+    $("#msgdv").show();
+    document.getElementById('nm').value="";
+    document.getElementById('eml').value="";
+    document.getElementById('comment').value="";
+    $("#msgdv").fadeOut(3000);
+    // Get data
+    // const getRequest = objectStore.get(1);
+    // getRequest.onsuccess = function(event) {
+    //     const user = event.target.result;
+    //     console.log(user.name); // Output: John Doe
+    //     console.log(user.email); // Output: john@example.com
+    // };
+};
+
+// Handle errors
+request.onerror = function(event) {
+    console.error("Database error: " + event.target.errorCode);
+};
+
+}
